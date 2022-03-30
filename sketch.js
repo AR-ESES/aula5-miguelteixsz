548afd08de0022c93eba6cb54c66eb354accc475
miguelpteix <miguelpteix@gmail.com>
2022-03-30 12:15:54 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,16 @@ function setup() {
 
 function draw() {
 
+	background(0);
+	if(mouseY >= height/2) {
+	noFill();
+	}
+	else{
+		fill(230);
+	}
+
 	stroke(230);
 	strokeWeight(2);
-	noFill();
 	circle(width / 2, height / 2, height / 4);
 
 
